657025b1f4d2723dd346d2a40b525eb63802a816
Bilash Halder <didibhai777@gmail.com>
2022-09-08 14:34:55 +0530

    Initial commit

---
+++ components/admin/AllAssociate.js
@@ -0,0 +1,7 @@
+import React from 'react'
+
+export default function AllAssociate() {
+  return (
+    <div>AllAssociate</div>
+  )
+}
---
+++ components/admin/Designation.js
@@ -0,0 +1,7 @@
+import React from 'react'
+
+export default function Designation() {
+  return (
+    <div>Designation</div>
+  )
+}
